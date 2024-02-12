5b93b940948b4c4360be1657e0e41e2ce588ce79
ferreiradev00 <renilson_fer@hotmail.com>
2024-02-12 13:04:34 -0300

    atualização readme

--- sum.test.js
+++ sum.test.js
@@ -1,7 +1,6 @@
 import sum from "./sum.js";
 import val_in from "./Testes.js";
 
+val_in(sum(1,2)).out_expected(3).coment('test sum passed');
 
-val_in(sum(1,2)).out_expected(3).coment('test sum');
-
-val_in(sum(1,2)).out_expected('3').coment('test sum');
+val_in(sum(1,2)).out_expected('3').coment('test sum error');
